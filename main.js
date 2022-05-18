a18c40d6c48269a5d65539a56ae7024c0ed0467d
omlonsane <90338824+omlonsane@users.noreply.github.com>
2022-05-18 21:03:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+noseX=0;
+noseY=0;
+
+function preload()
+{
+ clown_nose=loadImage("https://i.postimg.cc/65Yy0sFT/mustache-transparent-background-13.png");
+
+}
+
+function  setup()
+{
+   canvas= createCanvas(300,300);
+   canvas.center();
+   video= createCapture(VIDEO);
+   video.size(300,300);
+   video.hide();
+
+   poseNet=ml5.poseNet(video,modeloaded);
+   poseNet.on('pose',gotPoses);
+
+}
+function modeloaded()
+{
+   console.log("Posenet Is Initialized") ;
+}
+
+
+
+
+
+function gotPoses(results)
+{
+ if(results.length>0)
+ {
+    console.log(results);
+    noseX=results[0].pose.nose.x-35;
+    noseY=results[0].pose.nose.y-8;
+    console.log("nose x="+results[0].pose.nose.x);
+    console.log("nose y="+results[0].pose.nose.y);
+
+}
+}
+
+
+
+
+function draw()
+{
+   image(video,0,0,300,300);
+   image(clown_nose,noseX,noseY,70,50);
+
+  
+
+}
+
+function take_snapshot()
+{
+    save('background.jpg');
+    
+}
